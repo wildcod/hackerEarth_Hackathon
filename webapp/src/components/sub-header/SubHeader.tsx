895a65aa98362ba11b17470db8f5543d75aaf590
sahil <kanojias494@gmail.com>
2020-07-25 17:54:09 +0100

    added the modal

--- webapp/src/components/sub-header/SubHeader.tsx
+++ webapp/src/components/sub-header/SubHeader.tsx
@@ -1,28 +1,41 @@
 import React, { useState } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import './SubHeader.scss';
 
 const tabs = ['All', 'Personal', 'Work', 'Shopping', 'Others'];
 
 const SubHeader = () => {
   const [isSelected, setIsSelected] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="todo-sub-header">
       <div className="title">Schedule</div>
-      <div className="tabs">
-        {tabs.map((tab, index) => (
-          <div
-            className={isSelected === index ? 'selected' : ''}
-            onClick={() => setIsSelected(index)}>
-            {tab}
-          </div>
-        ))}
+      <div className="tabs-wrapper">
+        <div className="tabs-container">
+          {tabs.map((tab, index) => (
+            <div
+              className={isSelected === index ? 'selected' : ''}
+              onClick={() => setIsSelected(index)}>
+              {tab}
+            </div>
+          ))}
+        </div>
       </div>
       <div className="add-task">
-        <Button type="primary">
+        <Button type="primary" onClick={() => setIsModalOpen(true)}>
           <PlusOutlined /> Add Task
         </Button>
+        <Modal
+          title="Vertically centered modal dialog"
+          centered
+          visible={isModalOpen}
+          onOk={() => setIsModalOpen(false)}
+          onCancel={() => setIsModalOpen(false)}>
+          <p>some contents...</p>
+          <p>some contents...</p>
+          <p>some contents...</p>
+        </Modal>
       </div>
     </div>
   );
